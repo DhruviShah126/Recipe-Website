d1302723e9119d723a363b34590fcf1ff63473ae
Dhruvi Shah <dhruvishah549@gmail.com>
2023-01-25 20:22:12 -0500

    Added view all categories page and displayed current categories there; Connected with MongoDB and used it to display categories and their images

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,24 @@
+require('../models/database');
+const Category = require('../models/Category');
+
 exports.homePage = async(req, res) => {
-    res.render('home', {title: 'Recipes - Home Page'});
+    try {
+        const numCategories = 5;
+        const categories = await Category.find({}).limit(numCategories);
+
+        res.render('home', {title: 'Recipes - Home Page', categories});
+    } catch(error) {
+        res.status(500).send({message: error.message || "Error Occurred"});
+    }
+}
+
+exports.allCategories = async(req, res) => {
+    try {
+        const numCategories = 20;
+        const categories = await Category.find({}).limit(numCategories);
+
+        res.render('categories', {title: 'Recipes - All Categories', categories});
+    } catch(error) {
+        res.status(500).send({message: error.message || "Error Occurred"});
+    }
 }
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recipeController = require('../controllers/recipeController.js');
 
 router.get('/', recipeController.homePage);
+router.get('/categories', recipeController.allCategories);
 
 module.exports = router;
