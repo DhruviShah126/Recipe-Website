fc1f6ccaf11426a0c2b33506631efa157931f9b1
Dhruvi Shah <dhruvishah549@gmail.com>
2023-08-22 18:51:53 -0400

    Added page for each recipe

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -2,6 +2,9 @@ require('../models/database');
 const Category = require('../models/Category');
 const Recipe = require('../models/Recipe');
 
+/*
+    PATH: /
+*/
 exports.homePage = async(req, res) => {
     try {
         const limitNum = 5;
@@ -18,11 +21,27 @@ exports.homePage = async(req, res) => {
     }
 }
 
+/*
+    PATH: /categories
+*/
 exports.allCategories = async(req, res) => {
     try {
         const numCategories = 10;
         const categories = await Category.find({}).limit(numCategories);
         res.render('categories', {title: 'Recipes - All Categories', categories});
+    } catch(error) {
+        res.status(500).send({message: error.message || "Error Occurred"});
+    }
+}
+
+/*
+    PATH: /recipe/:id
+*/
+exports.exploreRecipes = async(req, res) => {
+    try {
+        let recipeId = req.params.id;
+        const recipe = await Recipe.findById(recipeId);
+        res.render('recipe', {title: 'Recipe', recipe});
     } catch(error) {
         res.status(500).send({message: error.message || "Error Occurred"});
     }
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,5 +4,6 @@ const recipeController = require('../controllers/recipeController.js');
 
 router.get('/', recipeController.homePage);
 router.get('/categories', recipeController.allCategories);
+router.get('/recipe/:id', recipeController.exploreRecipes);
 
 module.exports = router;
